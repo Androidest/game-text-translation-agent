21834f94b7844a487b1a4e7498366eb979b12d36
Androidest <stcoda@outlook.com>
2025-07-26 12:19:18 +0800

    Enhance term alignment instruction clarity. Fix term alignment dataset cleaner
    Signed-off-by: Androidest <stcoda@outlook.com>

--- 2_term _alignment/1_clean_data.py
+++ 2_term _alignment/1_clean_data.py
@@ -5,12 +5,22 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 from utils import *
 from tqdm import tqdm
 
-src = Sheet("../data/term_extraction.final.xlsx")
-cleaned = Sheet("../data/term_extraction.final.cleaned.xlsx", default_data={ "CN": [], "TERMS": [], "ES": [] }, clear=True)
+INPUT_PATH = "../data/term_extraction.final.xlsx"
+OUTPUT_PATH = "../data/term_extraction.final.cleaned.xlsx"
+
+src = Sheet(INPUT_PATH)
+cleaned = Sheet(OUTPUT_PATH, default_data={ "CN": [], "TERMS": [], "ES": [] }, clear=True)
+
+term_groups = set()
 for i, (cn, terms, es) in tqdm(enumerate(src), desc="Cleaning Data:"):
     if terms == "[]":
         continue
+    if terms in term_groups:
+        continue
+
+    term_groups.add(terms)
     cleaned.append({ "CN": cn, "TERMS": terms, "ES": es })
+
 cleaned.save()
-print(f"Done! Total cleaned data: {len(cleaned)}")
-print(f"Saved to file {"../data/term_extraction.final.cleaned.xlsx"}")
+print(f"Done! Total samples after cleaning: {len(cleaned)}")
+print(f"Saved to file {OUTPUT_PATH}")
