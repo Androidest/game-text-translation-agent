d3437b2d419c3d6c8293282407af578887dd4cd8
Androidest <stcoda@outlook.com>
2025-08-06 16:33:05 +0800

    use the new tokenizer based term extractor for the term data correction
    Signed-off-by: Androidest <stcoda@outlook.com>

--- p1_term_extraction/agent/_4_final_data_correction.py
+++ p1_term_extraction/agent/_4_final_data_correction.py
@@ -1,32 +1,25 @@
 #%% 
 from utils import *
-import jieba
+from p1_term_extraction import TokenizerBasedTermExtractor
 import json
 from tqdm import tqdm
 
 TERM_SCORE_FINAL_PATH = PATH_DATA / "term_score.final.xlsx"
-TERM_EXTRACTOR_DS_FINAL_PATH = PATH_DATA / "term_extraction.final.xlsx"
-TERM_EXTRACTOR_DS_PATH = PATH_DATA / "term_extraction.xlsx"
+INPUT_PATH = PATH_DATA / "term_extraction.xlsx"
+OUTPUT_PATH = PATH_DATA / "term_extraction.final.xlsx"
 
-term_score_final = Sheet(TERM_SCORE_FINAL_PATH)
-term_ds_final = Sheet(TERM_EXTRACTOR_DS_FINAL_PATH, default_data={ "CN":[], "TERMS":[], "ES":[] }, clear=True)
-terms_ds = Sheet(TERM_EXTRACTOR_DS_PATH)
+term_extract_tokenizer = TokenizerBasedTermExtractor(TERM_SCORE_FINAL_PATH)
+input_sheet = Sheet(INPUT_PATH)
+output_sheet = Sheet(OUTPUT_PATH, conlumns=input_sheet.column_names(), clear=True)
 
-tokenizer = jieba.Tokenizer()
-tokenizer.FREQ = {}
-term_set = set()
-for term, score in tqdm(term_score_final, desc="Loading Terms:"):
-    tokenizer.add_word(term, score)
-    term_set.add(term)
-
-for cn, _, es in tqdm(terms_ds, desc="Correcting Extraction:"):
-    terms = set([t for t in tokenizer.lcut(cn) if t in term_set])
-    terms = json.dumps(list(terms), ensure_ascii=False)
-    term_ds_final.append({
+for cn, old_terms, es in tqdm(input_sheet, desc="Correcting Extraction:"):
+    corrected_terms = term_extract_tokenizer.extract(cn)
+    corrected_terms = json.dumps(list(corrected_terms), ensure_ascii=False)
+    output_sheet.append({
         "CN": cn,
-        "TERMS": terms,
+        "TERMS": corrected_terms,
         "ES": es
     })
 
-term_ds_final.save()
-print(f"Correction Done! Saved to file: {TERM_EXTRACTOR_DS_FINAL_PATH}")
+output_sheet.save()
+print(f"Correction Done! Saved to file: {OUTPUT_PATH}")
