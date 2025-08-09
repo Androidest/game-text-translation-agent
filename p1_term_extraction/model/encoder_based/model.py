0847fbdb5e6d4b545db226584057c609d3145832
Androidest <stcoda@outlook.com>
2025-08-09 14:43:02 +0800

    Add GameTermNERDataset, GameTermBertTokenizer, GameTermBert
    Signed-off-by: Androidest <stcoda@outlook.com>

--- p1_term_extraction/model/encoder_based/model.py
+++ p1_term_extraction/model/encoder_based/model.py
@@ -5,8 +5,12 @@ import torch
 from typing import List, Dict, Tuple, Optional, Union
 import numpy as np
 
-MODEL_PATH = PATH_MODELS / "chinese-macbert-base"
-SAVE_PATH = PATH_MODELS / "fine-tuned-macbert-game-term-ner"
+TermList = List[str]
+LabelIds = List[int]
+ModelInputDict = Dict[str, torch.Tensor]
+BIOffsetMap = List[Tuple[int,int]]
+TermOffsetMap = List[Tuple[int,int]]
+
 LABEL_DICT:Dict[str, int] = {
     "B":0,
     "I":1,
@@ -20,19 +24,25 @@ class GameTermBertTokenizer(BertTokenizerFast):
     def convert_labels_to_ids(
         self, 
         labels:Union[str, List[str]], 
-        offsets_mapping:Union[List[Tuple[int,int]], List[List[Tuple[int,int]]]], 
+        offsets_mapping:Union[BIOffsetMap, List[BIOffsetMap]], 
         return_tensors:bool=False,
-    ) -> Union[torch.Tensor, List[List[int]]]:
+        max_len:int=None
+    ) -> Union[torch.Tensor, List[LabelIds]]:
         
-        if isinstance(labels, str):
+        if isinstance(labels, str) and len(offsets_mapping) == 1:
             labels = [labels]
+            if max_len is None:
+                max_len = len(offsets_mapping[0])
         
         results = []
         for l, o in zip(labels, offsets_mapping):
-            results.append(self._tokenize_labels(l, o))
+            results.append(self._tokenize_labels(l, o, max_len))
 
-        results = np.array(results)
-        if return_tensors:
+        if return_tensors and not max_len:
+            raise ValueError("'max_len' must be set when return_tensors=True")
+
+        if max_len and return_tensors:
+            results = np.array(results)
             return torch.tensor(results, dtype=torch.int)
 
         return results
@@ -40,13 +50,18 @@ class GameTermBertTokenizer(BertTokenizerFast):
     def _tokenize_labels(
         self, 
         labels:str, 
-        offsets_mapping:List[Tuple[int,int]], 
-    ) -> List[List[int]]:
+        offsets_mapping:BIOffsetMap, 
+        max_len:int=None,
+    ) -> List[LabelIds]:
+        
+        if max_len:
+            if max_len - len(offsets_mapping) < 0:
+                raise ValueError(f"Tokenized label length={len(offsets_mapping)} exceeds max_len={max_len}!")
         
         ids = []
         for start, end in offsets_mapping:
             if start == 0 and end == 0:
-                ids.append(-100)
+                ids.append(-100) # ignore index, the cross entopy function would ignore this label
                 continue
 
             label_name = labels[start]
@@ -56,15 +71,19 @@ class GameTermBertTokenizer(BertTokenizerFast):
 
             label_id = LABEL_DICT[label_name]
             ids.append(label_id)
+
+        if max_len and len(offsets_mapping) < max_len:
+            ids += [-100] * (max_len - len(offsets_mapping))
         
         return ids
 
     def convert_label_ids_to_terms(
         self,
-        label_ids:Union[torch.Tensor, List[List[int]]], 
+        label_ids:Union[torch.Tensor, List[LabelIds]], 
         input_text:Union[str, List[str]], 
-        offsets_mapping:Union[List[Tuple[int,int]], List[List[Tuple[int,int]]]], 
-    ):
+        offsets_mapping:Union[BIOffsetMap, List[BIOffsetMap]], 
+    )-> List[TermList]:
+        
         if isinstance(input_text, str):
             input_text = [input_text]
 
@@ -79,10 +98,10 @@ class GameTermBertTokenizer(BertTokenizerFast):
     
     def _extract_terms(
         self, 
-        label_ids:List[int],
+        label_ids:LabelIds,
         input_text:str, 
-        offsets_mapping:List[Tuple[int,int]], 
-    ) -> List[List[int]]:
+        offsets_mapping:BIOffsetMap, 
+    ) -> TermList:
         
         terms = []
         cur_term = ""
@@ -108,7 +127,45 @@ class GameTermBertTokenizer(BertTokenizerFast):
             terms.append(cur_term)
 
         return terms
+    
+    def convert_label_ids_to_term_offsets(
+        self, 
+        label_ids:Union[torch.Tensor, List[LabelIds]]
+    )-> List[TermOffsetMap]:
+        
+        results = []
+        for l in label_ids:
+            results.append(self._convert_label_ids_to_term_offsets(l))
+        return results
 
+    def _convert_label_ids_to_term_offsets(
+        self, 
+        label_ids:List[LabelIds]
+    )-> TermOffsetMap:
+        
+        start = None
+        offset_map = []
+        for index, label_id in enumerate(label_ids):
+            if label_id == -100:
+                continue
+
+            if label_id == LABEL_DICT['B']:
+                if start != None:
+                    offset_map.append((start, index))
+                start = index
+            elif label_id == LABEL_DICT['I']:
+                if start == None:
+                    continue
+            elif label_id == LABEL_DICT['O']:
+                if start != None:
+                    offset_map.append((start, index))
+                    start = None
+
+        if start != None:
+            offset_map.append((start, len(label_ids)))
+
+        return offset_map
+        
 class GameTermBert(BertPreTrainedModel):
     def __init__(self, config:BertConfig):
         super().__init__(config=config)
@@ -137,7 +194,7 @@ class GameTermBert(BertPreTrainedModel):
         logits = self.classifier(x)
 
         loss = None
-        if labels:
+        if labels is not None:
             loss = torch.nn.CrossEntropyLoss()(logits.view(-1, len(LABEL_DICT)), labels.view(-1))
 
         return SequenceClassifierOutput(
@@ -148,6 +205,8 @@ class GameTermBert(BertPreTrainedModel):
         )
 
 if __name__ == "__main__":
+    MODEL_PATH = PATH_MODELS / "chinese-macbert-base"
+    SAVE_PATH = PATH_MODELS / "fine-tuned-macbert-game-term-ner"
     sheet = Sheet(PATH_DATA/'term_extraction_train.xlsx')
     model:GameTermBert  = GameTermBert.from_pretrained(MODEL_PATH)
     tokenizer:GameTermBertTokenizer = GameTermBertTokenizer.from_pretrained(MODEL_PATH)
@@ -159,9 +218,11 @@ if __name__ == "__main__":
     print()
 
     x = tokenizer(text, return_offsets_mapping=True, return_tensors="pt")
-    label_ids = tokenizer.convert_labels_to_ids(labels, offsets_mapping=x['offset_mapping'], return_tensors=True)
-    print("label_ids:", label_ids.numpy())
+    label_ids = tokenizer.convert_labels_to_ids(labels, offsets_mapping=x['offset_mapping'], return_tensors=True, max_len=250)
+    print("Label ids:", label_ids.numpy())
     extracted_terms = tokenizer.convert_label_ids_to_terms(label_ids, input_text=text, offsets_mapping=x['offset_mapping'])
-    print("extracted_terms:", extracted_terms)
+    print("Extracted terms:", extracted_terms)
+    offset = tokenizer.convert_label_ids_to_term_offsets(label_ids)
+    print("Term offset:", offset)
 
     y = model(input_ids=x["input_ids"], attention_mask=x["attention_mask"])
